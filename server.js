2ff09486aca81e8830792b731080d3a5e18e3295
rlysam-feecee <sam17.bello@ymail.com>
2022-06-13 14:16:32 +0800

    Initial Commit, working sample from tutorial | backend CRUD and MongoDB connections

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const foodRouter = require("./routes/foodRoutes.js");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(
+	// yung database name after ng "... .net/"
+  "mongodb+srv://sam123:sam123@cluster0.fpb7h.mongodb.net/eye_management_system?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(foodRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running...");
+});
