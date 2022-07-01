8b2791dc64c34aad6315606fd54609b5e7864c83
rlysam-feecee <sam17.bello@ymail.com>
2022-07-01 15:14:55 +0800

    Eye History and Purchase History

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const itemRouter = require(path.join(__dirname, "./routes/itemRouters.js"));
 const userRouter = require(path.join(__dirname, "./routes/userRouters.js"));
 const appointmentRouter = require(path.join(__dirname, "./routes/appointmentRouters.js"));
 const cartRouter = require(path.join(__dirname, "./routes/cartRouters.js"));
+const purchaseHistoryRouter = require(path.join(__dirname, "./routes/purchaseHistoryRouters.js"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -34,6 +35,7 @@ app.use("/api/",itemRouter);
 app.use("/api/", userRouter);
 app.use("/api/", appointmentRouter);
 app.use("/api/", cartRouter);
+app.use("/api/", purchaseHistoryRouter);
 
 app.use(express.json());
 // app.use(express.static(path.join(__dirname), "/assets"));
